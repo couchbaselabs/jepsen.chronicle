578153820dec7ea73c0ea6ea6c798ca038e82832
Sven Signer <sven.signer@couchbase.com>
2020-12-07 16:08:27 +0000

    Expand CLI options
    Add CLI options to control the client operation generator.

--- src/chronicle/cli.clj
+++ src/chronicle/cli.clj
@@ -1,7 +1,26 @@
 (ns chronicle.cli
   (:require [chronicle.util :as util]))
 
+(defn parse-int
+  "Parse a string to an integer"
+  [x]
+  (Integer/parseInt x))
+
 (def extra-cli-opts
-  [[nil "--install SRC_DIR"
+  [[nil "--workload WORKLOAD"
+    "The workload to run"]
+   [nil "--doc-threads THREADS"
+    "Number of worker threads per key"
+    :default 3
+    :parse-fn parse-int]
+   [nil "--doc-op-limit LIMIT"
+    "Maximum number of ops to be executed before retiring a key. Set to 0 to never retire keys"
+    :default 0
+    :parse-fn parse-int]
+   [nil "--rate RATE"
+    "Limit the rate of operations to approximately rate operations per second. Set to 0 to disable rate limiting"
+    :default 0
+    :parse-fn parse-int]
+   [nil "--install SRC_DIR"
     "Copy the given source directory onto the test nodes and compile it prior to starting the test. If this argument is not provided then a previously compile copy must already be present on the nodes"
     :parse-fn util/get-install-package]])
