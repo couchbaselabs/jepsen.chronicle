27352751416861420325b4f47525d4cb7140e23b
Sven Signer <sven.signer@couchbase.com>
2021-03-01 16:20:33 +0000

    Wipe nodes after removing from the cluster
    Wipe nodes after removing them from the cluster, else they cannot be
    re-added. Wait 3 seconds before wiping to allow behaviour of removed
    nodes to be observed.

--- src/chronicle/util.clj
+++ src/chronicle/util.clj
@@ -62,7 +62,10 @@
   (http/post (str "http://" rest-target ":8080/config/removenode")
              {:body (str "\"chronicle_0@" node "\"")
               :content-type :json
-              :throw-entire-message? true}))
+              :throw-entire-message? true})
+  (Thread/sleep 3000)
+  (http/post (str "http://" node ":8080/node/wipe")
+             {:throw-entire-message? true}))
 
 (defn setup-cluster
   [test primary_node]
