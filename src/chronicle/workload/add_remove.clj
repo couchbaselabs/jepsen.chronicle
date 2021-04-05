e8c18b0ecd1cf32d73f03d7c56463d88ec1e5cad
Sven Signer <sven.signer@couchbase.com>
2021-04-05 10:55:43 +0100

    Update add-remove workload

--- src/chronicle/workload/add_remove.clj
+++ src/chronicle/workload/add_remove.clj
@@ -1,21 +1,25 @@
 (ns chronicle.workload.add-remove
   (:require [chronicle
              [nemesis :as nemesis]
+             [util :as util]
              [workload-util :as workload-util]]
             [jepsen
              [generator :as gen]]))
 
-(defn nemesis-gen
-  []
-  [(gen/sleep 5)
-   {:type :info :f :start}
-   (gen/sleep 5)
-   {:type :info :f :stop}
-   (gen/sleep 10)])
+(defn nemesis-gen-cycle-fn
+  [test ctx]
+  (let [node (util/get-one-ok-node test)]
+    [(gen/sleep 5)
+     {:type :info :f :remove :value [node]}
+     (gen/sleep 3)
+     {:type :info :f :wipe :value [node]}
+     (gen/sleep 10)
+     {:type :info :f :join :value [node]}
+     (gen/sleep 5)]))
 
 (defn add-remove-workload
   [opts]
   {:nemesis (nemesis/node-removal)
-   :generator (gen/nemesis
-               (nemesis-gen)
-               (gen/time-limit 45 (workload-util/client-gen opts)))})
+   :generator (->> (workload-util/client-gen opts)
+                   (gen/nemesis nemesis-gen-cycle-fn)
+                   (gen/time-limit (:time-limit opts)))})
