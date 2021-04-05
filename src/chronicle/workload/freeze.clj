327240723939596312908e7b8eb8eda629bfbb28
Sven Signer <sven.signer@couchbase.com>
2021-04-05 10:55:43 +0100

    Update freeze workload

--- src/chronicle/workload/freeze.clj
+++ src/chronicle/workload/freeze.clj
@@ -1,21 +1,22 @@
 (ns chronicle.workload.freeze
   (:require [chronicle
              [nemesis :as nemesis]
+             [util :as util]
              [workload-util :as workload-util]]
             [jepsen
              [generator :as gen]]))
 
-(defn nemesis-gen
-  []
-  [(gen/sleep 5)
-   {:type :info :f :start}
-   (gen/sleep 5)
-   {:type :info :f :stop}
-   (gen/sleep 10)])
+(defn nemesis-gen-cycle-fn
+  [test ctx]
+  (let [node (util/get-one-ok-node test)]
+    [(gen/sleep 5)
+     {:type :info :f :freeze :value [node]}
+     (gen/sleep 10)
+     {:type :info :f :resume :value [node]}]))
 
 (defn freeze-workload
   [opts]
   {:nemesis (nemesis/node-freeze)
-   :generator (gen/nemesis
-               (nemesis-gen)
-               (gen/time-limit 45 (workload-util/client-gen opts)))})
+   :generator (->> (workload-util/client-gen opts)
+                   (gen/nemesis nemesis-gen-cycle-fn)
+                   (gen/time-limit (:time-limit opts)))})
