cad9cd10b66413824bc4710ff6114018a8693d18
Sven Signer <sven.signer@couchbase.com>
2021-08-02 16:09:26 +0100

    Update mixed workload
    Update the mixed workload to allow simultaneous node failure and
    network partitioning to be applied to a node.

--- src/chronicle/workload/mixed.clj
+++ src/chronicle/workload/mixed.clj
@@ -18,21 +18,38 @@
         max-minority (quot (dec test-nodes) 2)
         can-disrupt  (< disrupted max-minority)
 
-        actions (keep
-                 (fn [[node status]]
-                   (case status
-                     #{} (if can-disrupt
-                           {:type :info
-                            :value [node]
-                            :f (rand-nth [:remove :crash :freeze])})
+        actions (reduce
+                 (fn [actions [node status]]
+                   (concat
+                    actions
+                    (map (fn [f] {:type :info :f f :value [node]})
+                         (case status
+                           ;; Disrupt healthy
+                           #{} (if can-disrupt
+                                 [:remove :crash :freeze :isolate-completely])
 
-                     ;; Repair Removal
-                     #{:removed} {:type :info :value [node] :f :wipe}
-                     #{:removed :wiped} {:type :info :value [node] :f :join}
+                           ;; Repair removal, partitioning after removal is
+                           ;; unlikely to be interesting
+                           #{:removed} [:wipe]
+                           #{:removed :partitioned} [:wipe]
+                           #{:removed :wiped} [:join]
+                           ;; Need to heal before attempting to rejoin
+                           #{:removed :partitioned :wiped} [:heal-network]
 
-                     ;; Repair crash and freeze
-                     #{:killed} {:type :info :value [node] :f :restart}
-                     #{:frozen} {:type :info :value [node] :f :resume}))
+                           ;; Repair crash, add partition
+                           #{:killed} [:restart :isolate-completely]
+                           #{:killed :frozen} [:restart :isolate-completely]
+                           #{:killed :partitioned} [:restart :heal-network]
+                           #{:killed :frozen :partitioned} [:restart]
+                           ;; Unfreeze, crash, or add partition
+                           #{:frozen} [:resume :isolate-completely :crash]
+                           #{:frozen :partitioned} [:resume :heal-network :crash]
+
+                           ;; Repair partition, or add some other disruption
+                           #{:partitioned} [:remove :crash :freeze :heal-network]
+
+                           []))))
+                 []
                  @(:membership test))]
     (if (empty? actions)
       (throw+ {:error "No valid actions?"
@@ -45,7 +62,8 @@
   {:nemesis (compose
              {#{:remove :wipe :join} (nemesis/node-removal)
               #{:crash :restart} (nemesis/node-crash)
-              #{:freeze :resume} (nemesis/node-freeze)})
+              #{:freeze :resume} (nemesis/node-freeze)
+              #{:isolate-completely :heal-network} (nemesis/network-partition)})
    :generator (->> (workload-util/client-gen opts)
                    (gen/nemesis nemesis-gen)
                    (gen/time-limit (:time-limit opts)))})
